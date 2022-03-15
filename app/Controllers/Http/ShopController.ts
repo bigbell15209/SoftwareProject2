ebcec7f7a5f335c2549d4df6d5b44014285c1ff9
Max Martin <mmart180@my.centennialcollege.ca>
2022-03-15 12:07:53 -0400

    Created shop controller

---
+++ app/Controllers/Http/ShopController.ts
@@ -0,0 +1,7 @@
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+const mongoose = require("mongoose");
+const Shop = mongoose.model("Shop");
+
+export default class ShopController {
+
+}
