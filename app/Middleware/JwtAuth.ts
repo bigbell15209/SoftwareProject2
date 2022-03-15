6973cf5e12567fdbd4c845d35e94e070559130de
Max Martin <mmart180@my.centennialcollege.ca>
2022-03-15 12:06:16 -0400

    Created JWT authentication middleware

--- app/Middleware/JwtAuth.ts
+++ app/Middleware/JwtAuth.ts
@@ -1,16 +1,18 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 const jwt = require("jsonwebtoken");
 import Env from "@ioc:Adonis/Core/Env";
-
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 export default class JwtAuth {
   public async handle(
     { request, response }: HttpContextContract,
     next: () => Promise<void>
   ) {
-    const token  = request.headers().authorization?.replace("Bearer ", "");
+    const token = request.headers().authorization?.replace("Bearer ", "");
     if (!token || !jwt.verify(token, Env.get("JWT_SECRET")))
-      return response.unauthorized({error: "Invalid token."})
+      return response.unauthorized({ error: "Invalid token." });
+    request["user"] = await User.findOne({ userId: jwt.decode(token).sub });
     await next();
   }
 }
