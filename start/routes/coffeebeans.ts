140ab19ad087ea5b5f7e0a3b5972e9e392426005
Max Martin <mmart180@my.centennialcollege.ca>
2022-03-27 12:34:09 -0400

    added bean details

--- start/routes/coffeebeans.ts
+++ start/routes/coffeebeans.ts
@@ -4,6 +4,7 @@ Route.group(() => {
   Route.get("/", "CoffeeBeansController.getAll");
   Route.post("create", "CoffeeBeansController.create");
   Route.post("update", "CoffeeBeansController.update");
+  Route.get("details", "CoffeeBeansController.details");
   Route.post("delete", "CoffeeBeansController.delete");
 })
   .prefix("/coffeebeans")
