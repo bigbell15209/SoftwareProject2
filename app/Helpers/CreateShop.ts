1455eae00ced56f2021f37e209db08313bab6abf
Max Martin <mmart180@my.centennialcollege.ca>
2022-03-27 12:33:37 -0400

    updated shop location

--- app/Helpers/CreateShop.ts
+++ app/Helpers/CreateShop.ts
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose");
 const Shop = mongoose.model("Shop");
+import { v4 as uuid } from "uuid";
 
 export default async function createShop(ownerId: string, shop: any) {
-  const { name, description, address, image, coords, deliveryRange } = shop;
+  const { name, description, address, image, location, deliveryRange } = shop;
   try {
     const newShop = await Shop.create({
+      shopId: uuid(),
       name,
       description,
       address,
       image,
       location: {
-        type: "Point",
-        coordinates: [coords.lng, coords.lat],
+        lat: location.lat,
+        lng: location.lng,
       },
       userId: ownerId,
       deliveryRange,
