f7ce36ce2d76cd8de7da641618f2c6c685d9961d
Max Martin <mmart180@my.centennialcollege.ca>
2022-03-27 12:32:48 -0400

    updated shop details

--- app/Controllers/Http/ShopsController.ts
+++ app/Controllers/Http/ShopsController.ts
@@ -1,26 +1,49 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 const mongoose = require("mongoose");
 const Shop = mongoose.model("Shop");
+const CoffeeBean = mongoose.model("CoffeeBean");
+import { getDistance } from "geolib";
 
 export default class ShopsController {
   public async getNearbyShops({ request, response }: HttpContextContract) {
-    const { lat, lng } = request.qs();
-    const nearbyShops = await Shop.find({
-      location: {
-        $near: {
-          $geometry: {
-            type: "Point",
-            coordinates: [lng, lat],
-          },
-          $maxDistance: 100_000,
-          $minDistance: 0,
+    let { lat, lng } = request.qs();
+    lat = parseFloat(lat);
+    lng = parseFloat(lng);
+
+    const allShops = await Shop.find();
+    const shops: object[] = [];
+    for (let i = 0; i < allShops.length; i++) {
+      const beans = (await CoffeeBean.find({ shopId: allShops[i].shopId }))
+      .map(bean => ({
+        coffeeBeanId: bean.coffeeBeanId,
+        shopId: bean.shopId,
+        name: bean.name,
+        description: bean.description,
+        price: bean.price,
+        specie: bean.specie,
+        origin: bean.origin,
+        roastingLevel: bean.roastingLevel,
+      }))
+      const shop = {
+        shopId: allShops[i].shopId,
+        name: allShops[i].name,
+        description: allShops[i].description,
+        address: allShops[i].address,
+        location: {
+          lat: allShops[i].location.lat,
+          lng: allShops[i].location.lng,
         },
-      },
-    });
-
-    console.log(nearbyShops)
-
-
-    return nearbyShops
+        distance: getDistance(
+          { latitude: lat, longitude: lng },
+          {
+            latitude: allShops[i].location.lat,
+            longitude: allShops[i].location.lng,
+          }
+        ),
+        beans,
+      };
+      shops.push(shop);
+    }
+    return shops;
   }
 }
