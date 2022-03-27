aa483842eaebf866eac552a9e015d4fde54b33ad
Max Martin <mmart180@my.centennialcollege.ca>
2022-03-27 12:32:06 -0400

    updated routes

--- start/routes/debug.ts
+++ start/routes/debug.ts
@@ -3,5 +3,4 @@ import Route from "@ioc:Adonis/Core/Route";
 Route.group(() => {
   Route.post("/signup", "AuthController.signUp");
   Route.post("/signin", "AuthController.signIn");
-  Route.get("/signout", "AuthController.signOut");
 }).prefix("/auth");
